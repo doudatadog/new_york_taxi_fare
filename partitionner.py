45166de1b003d825ead58d65a8423b3564a05d6a
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-04-05 16:10:50 +0200

    first commit

--- partitionner.py
+++ partitionner.py
@@ -3,22 +3,23 @@
 import pandas,tqdm
 
 
-chunksize       = 1e5 #Data will be loaded in chunks
-i               = 0 # iteration step
+chunksize                  = 1e5 #Data will be loaded in chunks
+i                          = 0 # iteration step
 
 for chunk in tqdm.tqdm(
 
     pandas.read_csv(
-        './data/train.csv',
-        chunksize         =chunksize,
-        parse_dates       =['pickup_datetime']
-                        )):
-    i +=1
+        './data/train.csv'  ,
+        chunksize           = chunksize,
+        parse_dates         = ['pickup_datetime'] #Automatically parse input to Datetime64[ns] format
+                        )   ):
+    i +=1 #update step
     chunk.to_hdf(
-        './data/train/{i}.h5'.format(i=i),
-                     index=None,
-                       key='table',
-                      mode='w') #storing chunk to Hdf Format
+        './data/train/{i}.h5'.format(
+                            i=i),
+                     index   =None,
+                       key   ='table',
+                      mode   ='w') #storing chunk to Hdf Format which is faster for read Ops
     chunk.assign(
     
             dist = lambda DF : pandas.np.sqrt(pandas.np.sum(
@@ -26,13 +27,13 @@ for chunk in tqdm.tqdm(
                 DF[['pickup_longitude','pickup_latitude']
                 ].values - DF[
                 ['dropoff_longitude','dropoff_latitude']].values,2),
-                1)), #computing efficiently Euclidean distance between pick and drop points
-                dayofweek= lambda DF : DF.pickup_datetime.dt.dayofweek,
-                dayofyear= lambda DF : DF.pickup_datetime.dt.dayofyear,
-                hour     = lambda DF : DF.pickup_datetime.dt.hour,
-                month    = lambda DF : DF.pickup_datetime.dt.month,
-                weekofyear=lambda DF : DF.pickup_datetime.dt.weekofyear,
-                year      = lambda DF : DF.pickup_datetime.dt.year).round(
+                1))            , #computing efficiently Euclidean distance between pick and drop points
+                dayofweek      = lambda DF : DF.pickup_datetime.dt.dayofweek,
+                dayofyear      = lambda DF : DF.pickup_datetime.dt.dayofyear,
+                hour           = lambda DF : DF.pickup_datetime.dt.hour,
+                month          = lambda DF : DF.pickup_datetime.dt.month,
+                weekofyear      =lambda DF : DF.pickup_datetime.dt.weekofyear,
+                year            = lambda DF : DF.pickup_datetime.dt.year).round(
                 {'pickup_latitude': 2, 'pickup_longitude': 2}).groupby(
                 ['pickup_latitude','pickup_longitude','hour']
                 ).key.count().reset_index(
