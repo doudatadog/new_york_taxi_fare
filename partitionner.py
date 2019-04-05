71bb0ba357ddc15980b3340312f0d98633a623b0
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-04-05 16:06:44 +0200

    first commit

--- partitionner.py
+++ partitionner.py
@@ -1,15 +1,24 @@
-import pandas,geopandas,random,tqdm
+#!/usr/bin/env python2
 
-from scipy.spatial                                import distance
+import pandas,tqdm
 
 
-places = []
+chunksize       = 1e5 #Data will be loaded in chunks
+i               = 0 # iteration step
 
-chunksize = 1e5
-i = 0
-for chunk in tqdm.tqdm(pandas.read_csv('./data/train.csv',chunksize=chunksize,parse_dates     =['pickup_datetime'])):
+for chunk in tqdm.tqdm(
+
+    pandas.read_csv(
+        './data/train.csv',
+        chunksize         =chunksize,
+        parse_dates       =['pickup_datetime']
+                        )):
     i +=1
-    chunk.to_hdf('./data/train/{i}.h5'.format(i=i),index=None,key='table',mode='w')
+    chunk.to_hdf(
+        './data/train/{i}.h5'.format(i=i),
+                     index=None,
+                       key='table',
+                      mode='w') #storing chunk to Hdf Format
     chunk.assign(
     
             dist = lambda DF : pandas.np.sqrt(pandas.np.sum(
@@ -17,13 +26,18 @@ for chunk in tqdm.tqdm(pandas.read_csv('./data/train.csv',chunksize=chunksize,pa
                 DF[['pickup_longitude','pickup_latitude']
                 ].values - DF[
                 ['dropoff_longitude','dropoff_latitude']].values,2),
-                1)),
+                1)), #computing efficiently Euclidean distance between pick and drop points
                 dayofweek= lambda DF : DF.pickup_datetime.dt.dayofweek,
                 dayofyear= lambda DF : DF.pickup_datetime.dt.dayofyear,
                 hour     = lambda DF : DF.pickup_datetime.dt.hour,
                 month    = lambda DF : DF.pickup_datetime.dt.month,
                 weekofyear=lambda DF : DF.pickup_datetime.dt.weekofyear,
-                year      = lambda DF : DF.pickup_datetime.dt.year).round({'pickup_latitude': 2, 'pickup_longitude': 2}).groupby(
+                year      = lambda DF : DF.pickup_datetime.dt.year).round(
+                {'pickup_latitude': 2, 'pickup_longitude': 2}).groupby(
                 ['pickup_latitude','pickup_longitude','hour']
                 ).key.count().reset_index(
-                ).to_hdf('./data/traffic/{i}.h5'.format(i=i),index=None,key='table',mode='w')
+                ).to_hdf(
+        './data/traffic/{i}.h5'.format(i=i),
+                   index=None,
+                     key='table',
+                    mode='w') #Estimating Traffic per hour in pick up Area
