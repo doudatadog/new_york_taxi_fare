bb084226fec7a62f32f92eeef3524a425b4dcd04
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-04-14 02:53:52 +0200

    geomatch

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import pandas,os
+import pandas,os,datetime
 from scipy.spatial import distance
 
 
@@ -20,7 +20,75 @@ H5        = lambda  fp : [
 lon_bnd   = [-75.,-71.]
 lat_bnd   = [39.,42]
 boundaryQ = '(@lon_bnd[0]<pickup_longitude<@lon_bnd[1]) and (@lat_bnd[0]<pickup_latitude<@lat_bnd[1])'
-
+class Tic:
+    def __init__(self)          : 
+        self.tasks              = []
+    def New_task(self,desc)          :
+        self.desc               = desc
+        print(desc)
+        self.start              = datetime.datetime.now()
+    def launch(self)            :
+        self.Tdesc              = 'Lanched Process'
+        print(self.Tdesc)
+        self.Tstart              = datetime.datetime.now()
+        return self
+    def Toc(self):
+        end                     = datetime.datetime.now()
+        info                    = "Taskname : %s :\n\t\ttook me %s seconds" %(self.desc,end-self.start)
+        self.tasks.append(info)
+        print(info)
+    def finish(self)            :
+        self.start              = self.Tstart
+        self.desc               = self.Tdesc
+        self.Toc()
+class FeatureEngineerer:
+    def __init__(self,name) :
+        self.name           = name
+        self.pickgeo        = ['pickup_latitude','pickup_longitude']
+        self.dropgeo        = ['dropoff_latitude','dropoff_longitude']
+    def Read(self):
+        self.DF             = pandas.read_hdf(
+                              self.name,
+                       key  = 'table'
+        ).query(boundaryQ
+        ).reset_index(drop=True
+        ).drop(
+        ['key','pickup_datetime'],
+        axis=1
+        ).assign(
+        dist                 = lambda DF : pandas.np.sqrt(pandas.np.sum(
+        pandas.np.power(
+        DF[
+        self.pickgeo
+        ].values - DF[
+        self.dropgeo
+        ].values,2),1)) #computing efficiently Euclidean distance between pick and drop points
+        )
+    def MatchPopTraf(self,trafObj):
+        self.DF                   = self.DF.pipe(
+        lambda DF0                       : Link2DFS_by_nearestGeo(
+        DF1                       = DF0,
+        DF2                       = trafObj,
+        ColMapper                 = [self.pickgeo,['lat','lng']],
+        prefix                    = 'pickup')
+        ).pipe(
+        lambda DF1                : Link2DFS_by_nearestGeo(
+        DF1                       = DF1,
+        DF2                       = trafObj,
+        ColMapper                 = [self.dropgeo,['lat','lng']],
+        prefix                    = 'dropoff'))
+    def Process(self,trafObj)     :
+        clock                       = Tic().launch()
+        clock.New_task(
+        'Loading DF{}'.format(
+        self.name))
+        self.Read()
+        clock.Toc()
+        clock.New_task('Matching Pop')
+        self.MatchPopTraf(trafObj)
+        clock.Toc()
+        clock.finish()
+        return self.DF
 def Link2DFS_by_nearestGeo(
 	DF1,
 	DF2,
